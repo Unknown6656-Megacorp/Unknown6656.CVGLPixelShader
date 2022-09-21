344b4beab2f01c0d2f7e22e772851053de923a82
unknown6656 <unknown84@gmx.de>
2022-09-21 21:50:30 +0200

    added rgb split effects

--- Unknown6656/CVGL/Shaders/color_effects.py
+++ Unknown6656/CVGL/Shaders/color_effects.py
@@ -7,24 +7,23 @@ Invert = _generate_shader_class('out_color = mix(out_color, 1 - out_color, stren
 
 
 Grayscale = _generate_shader_class('''
-const vec3 gray = vec3(dot(out_color.rgb, vec3(0.299, 0.587, 0.114)));
-out_color.rgb = mix(out_color.rgb, gray, clamp(strength, 0, 1));
+const float gray = ciegray(out_color.rgb);
+out_color.rgb = mix(out_color.rgb, vec3(gray), clamp(strength, 0, 1));
 ''', '', [
     ShaderVariable('strength', ShaderVariableType.FLOAT, 1)
 ])
 
 
 Saturation = _generate_shader_class('''
-const vec3 gray = vec3(dot(out_color.rgb, vec3(0.299, 0.587, 0.114)));
-out_color.rgb = mix(gray, out_color.rgb, clamp(saturation, 0, 1000000));
+const float gray = ciegray(out_color.rgb);
+out_color.rgb = mix(vec3(gray), out_color.rgb, clamp(saturation, 0, 1000000));
 ''', '', [
     ShaderVariable('saturation', ShaderVariableType.FLOAT, 1)
 ])
 
 
 Sepia = _generate_shader_class('''
-float gray = dot(out_color.rgb, vec3(0.299, 0.587, 0.114));
-
+const float gray = ciegray(out_color.rgb);
 out_color.rgb = mix(out_color.rgb, gray * color, strength);
 ''', '', [
     ShaderVariable('color', ShaderVariableType.FLOAT_3, [1.2, 1.0, 0.8]),
@@ -43,34 +42,40 @@ Contrast = _generate_shader_class('out_color.rgb = (out_color.rgb - .5) * contra
 
 
 Hue = _generate_shader_class('''
-const float TAU = 6.28318530718;
 vec3 hsv = rgb2hsv(out_color.rgb);
 const float h = hsv.x + hue / TAU;
 
 hsv.x = h - floor(h);
 
 out_color.rgb = hsv2rgb(hsv);
-''', '''
-vec3 rgb2hsv(const vec3 c)
-{
-    const vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
-    const vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
-    const vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
-    const float d = q.x - min(q.w, q.y);
-    const float e = 1.0e-10;
-
-    return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
-}
-
-vec3 hsv2rgb(const vec3 c)
-{
-    const vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
-    const vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
-
-    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
-}
-''', [
+''', '', [
     ShaderVariable('hue', ShaderVariableType.FLOAT, 0)
+])
+
+
+RGBLinearSplit = _generate_shader_class('''
+const vec2 offs = vec2(
+    cos(direction) / width,
+    sin(direction) / height
+) * size;
+
+out_color.r = texture2D(image, coords - offs).r;
+out_color.b = texture2D(image, coords + offs).b;
+''', '', [
+    ShaderVariable('size', ShaderVariableType.FLOAT, 20),
+    ShaderVariable('direction', ShaderVariableType.FLOAT, 0),
+])
+
+
+RGBRadialSplit = _generate_shader_class('''
+vec2 scale = 1.0 + size / vec2(width, height);
+vec2 scale2 = 1.0 + size * 2.0 / vec2(width, height);
+
+out_color.g = texture2D(image, (coords - center) / scale + center).g;
+out_color.b = texture2D(image, (coords - center) / scale2 + center).b;
+''', '', [
+    ShaderVariable('size', ShaderVariableType.FLOAT, 20),
+    ShaderVariable('center', ShaderVariableType.FLOAT_2, [.5, .5]),
 ])
 
 
