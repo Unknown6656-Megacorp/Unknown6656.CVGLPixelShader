5855eab681b3904ecd18441d7c7bc120868f0e65
unknown6656 <unknown84@gmx.de>
2022-09-21 21:50:18 +0200

    added more blur effects

--- Unknown6656/CVGL/Shaders/blur.py
+++ Unknown6656/CVGL/Shaders/blur.py
@@ -1,19 +1,51 @@
 from .. import ShaderVariable, ShaderVariableType, _generate_shader_class
 
 
-Blur = _generate_shader_class('''
-const float TAU = 6.28318530718;
-float i_qual = 1.0 / quality;
-
-for (float phi = 0.0; phi < TAU; phi += TAU * i_qual * i_qual)
-    for (float i = i_qual; i <= 1.0; i += i_qual)
-        out_color += texture2D(image, coords + vec2(cos(phi) / width, sin(phi) / height) * radius * i);
-
-out_color *= i_qual * i_qual * i_qual;
-''', '', [
+Blur = _generate_shader_class('out_color = blur(coords, radius, quality);', '', [
     ShaderVariable('quality', ShaderVariableType.FLOAT, 5.5),
     ShaderVariable('radius', ShaderVariableType.FLOAT, 10)
 ])
+
+
+LinearBlur = _generate_shader_class('''
+float phi = direction;
+float i_qual = 1.0 / quality;
+vec2 scale = size * .5 / vec2(width, height);
+
+for (float i = -1.0; i <= 1.0; i += i_qual)
+{
+    vec2 c = coords + i * scale * vec2(cos(phi), sin(phi));
+    out_color += texture2D(image, c);
+}
+
+out_color /= 2 * quality + 1;
+''', '', [
+    ShaderVariable('size', ShaderVariableType.FLOAT, 10),
+    ShaderVariable('direction', ShaderVariableType.FLOAT, 0),
+    ShaderVariable('quality', ShaderVariableType.FLOAT, 5),
+])
+
+
+RadialBlur = _generate_shader_class('''
+float i_qual = 1.0 / quality;
+vec2 scale = size / vec2(width, height);
+
+for (float i = -2.0; i <= 2.0; i += i_qual)
+{
+
+    vec2 c = (coords - center) * (1 + scale * i) + center;
+    out_color += texture2D(image, c);
+}
+
+out_color /= 4 * quality + 1;
+''', '', [
+    ShaderVariable('size', ShaderVariableType.FLOAT, 10),
+    ShaderVariable('center', ShaderVariableType.FLOAT_2, [.5, .5]),
+    ShaderVariable('quality', ShaderVariableType.FLOAT, 5),
+])
+
+
+# TODO : concentric blur
 
 
 Bloom = _generate_shader_class('''
@@ -21,7 +53,6 @@ vec3 bloom = vec3(0.0);
 float i_qual = 1.0 / quality;
 
 const float EPS = 1.0e-10;
-const float TAU = 6.28318530718;
 const vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
 
 for (float phi = 0.0; phi < TAU; phi += TAU * i_qual * i_qual)
@@ -43,3 +74,24 @@ out_color.rgb += bloom * intensity;
     ShaderVariable('quality', ShaderVariableType.FLOAT, 6),
     ShaderVariable('radius', ShaderVariableType.FLOAT, 40),
 ])
+
+
+
+___TODO___ChromaticAbberation = _generate_shader_class('''
+#define BLUR 1
+
+const float scale_factor = 23.0 / max(width, height);
+vec2 scale = (1.0 + scale_factor) * vec2(height / width, 1);
+vec2 scale2 = (1.0 + scale_factor * 2.0) * vec2(height / width, 1);
+
+out_color.r = blur((coords - center) / scale + center, BLUR, quality).r;
+out_color.g = blur((coords - center) / scale2 + center, BLUR, quality).g;
+out_color.b = blur(coords, BLUR, quality).b;
+''', '', [
+    ShaderVariable('threshold', ShaderVariableType.FLOAT, 0.75),
+    ShaderVariable('quality', ShaderVariableType.FLOAT, 6),
+    ShaderVariable('radius', ShaderVariableType.FLOAT, 40),
+    
+    ShaderVariable('center', ShaderVariableType.FLOAT_2, [.5, .5]),
+])
+
